241cf5a71f11a243099f68c7dd65af9c1c4183e9
TARUN.V <tarun.v@TARUNVs-MacBook-Air.local>
2026-05-28 11:18:09 +0530

    final submission

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const public_users = require('./router/general');
 const regd_users = require('./router/auth_users');
+
 const app = express();
+
 app.use(bodyParser.json());
+
 app.use('/', public_users);
 app.use('/customer', regd_users);
-const PORT = 3000;
+
+// optional but good
+app.get('/', (req, res) => {
+    res.send("Book Review API is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
-})
+    console.log(`Server running on port ${PORT}`);
+});
